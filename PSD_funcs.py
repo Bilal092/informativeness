d119b4232edff0d3218f3544023f80e239d7f0bc
Bilal Riaz <34689025+Bilal092@users.noreply.github.com>
2021-04-27 16:04:18 -0400

    Add files via upload

--- PSD_funcs.py
+++ PSD_funcs.py
@@ -6,7 +6,6 @@ def is_PSD(X):
 
 
 def PSD2normedCorr(K):
-
     assert(len(K.shape) == 2), "input array must be square"
     assert(K.shape[0] == K.shape[1]), "Input matrix must be square"
 
@@ -23,20 +22,23 @@ def PSD2normedCorr(K):
 
 
 def Dist2Corr(D):
-  # Centering
-  HD = D - np.mean(D, axis=0)
-  HDH = HD - np.mean(HD, axis=1)[:, np.newaxis]
-  # Eigenvalue Truncation
-  [E, U] = np.linalg.eigh(-HDH)
-  E[E < 0] = 0
-  K = U@np.diag(E)@U.T
-  # Normalization for PSD to Correlation matrix
-  Diag = np.diag(K)
-  Diag_nonzeros = Diag > 0
-  D = np.sqrt(Diag)[:, np.newaxis]
-  K_Corr = np.zeros(K.shape)
-  K_Corr[Diag_nonzeros, :] = K[Diag_nonzeros, :]/D[Diag_nonzeros]
-  K_Corr[:, Diag_nonzeros] = K_Corr[:, Diag_nonzeros]/D[Diag_nonzeros].T
-  K_Corr = (K_Corr + K_Corr.T)/2
+    assert(len(D.shape) == 2), "input array must be square"
+    assert(D.shape[0] == D.shape[1]), "Input matrix must be square"
+    
+    # Centering
+    HD = D - np.mean(D, axis=0)
+    HDH = HD - np.mean(HD, axis=1)[:, np.newaxis]
+    # Eigenvalue Truncation
+    [E, U] = np.linalg.eigh(-HDH)
+    E[E < 0] = 0
+    K = U@np.diag(E)@U.T
+    # Normalization for PSD to Correlation matrix
+    Diag = np.diag(K)
+    Diag_nonzeros = Diag > 0
+    D = np.sqrt(Diag)[:, np.newaxis]
+    K_Corr = np.zeros(K.shape)
+    K_Corr[Diag_nonzeros, :] = K[Diag_nonzeros, :]/D[Diag_nonzeros]
+    K_Corr[:, Diag_nonzeros] = K_Corr[:, Diag_nonzeros]/D[Diag_nonzeros].T
+    K_Corr = (K_Corr + K_Corr.T)/2
 
-  return K_Corr
+    return K_Corr
