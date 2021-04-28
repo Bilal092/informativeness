779bd304beea83588e9c382b81553a277498667c
Bilal Riaz <34689025+Bilal092@users.noreply.github.com>
2021-04-28 01:13:27 -0400

    Add files via upload

--- PSD_funcs.py
+++ PSD_funcs.py
@@ -6,25 +6,27 @@ def is_PSD(X):
 
 
 def PSD2normedCorr(K):
+
     assert(len(K.shape) == 2), "input array must be square"
     assert(K.shape[0] == K.shape[1]), "Input matrix must be square"
-
     N = K.shape[0]
     Diag = np.diag(K)
+    Diag = np.diag(K)
     Diag_nonzeros = Diag > 0
-    D = np.sqrt(Diag)[:, np.newaxis]
     K_Corr = np.zeros(K.shape)
-    K_Corr[Diag_nonzeros, :] = K[Diag_nonzeros, :]/D[Diag_nonzeros]
-    K_Corr[:, Diag_nonzeros] = K_Corr[:, Diag_nonzeros]/D[Diag_nonzeros].T
+    sqrt_inv_nz_Diag = np.zeros(Diag.shape)
+    sqrt_inv_nz_Diag[Diag_nonzeros] = 1 / np.sqrt(Diag[Diag_nonzeros])
+    K_Corr[Diag_nonzeros, :] = K[Diag_nonzeros, :] * sqrt_inv_nz_Diag[Diag_nonzeros][:, np.newaxis]
+    K_Corr[:, Diag_nonzeros] = K_Corr[:, Diag_nonzeros] * sqrt_inv_nz_Diag[Diag_nonzeros][np.newaxis, :]
+    np.fill_diagonal(K_Corr, 1)
     K_Corr = (K_Corr + K_Corr.T)/2
 
-    return 1/N*K_Corr
+    return K_Corr/N
 
 
 def Dist2Corr(D):
     assert(len(D.shape) == 2), "input array must be square"
     assert(D.shape[0] == D.shape[1]), "Input matrix must be square"
-    
     # Centering
     HD = D - np.mean(D, axis=0)
     HDH = HD - np.mean(HD, axis=1)[:, np.newaxis]
@@ -33,12 +35,16 @@ def Dist2Corr(D):
     E[E < 0] = 0
     K = U@np.diag(E)@U.T
     # Normalization for PSD to Correlation matrix
+    N = K.shape[0]
+    Diag = np.diag(K)
     Diag = np.diag(K)
     Diag_nonzeros = Diag > 0
-    D = np.sqrt(Diag)[:, np.newaxis]
     K_Corr = np.zeros(K.shape)
-    K_Corr[Diag_nonzeros, :] = K[Diag_nonzeros, :]/D[Diag_nonzeros]
-    K_Corr[:, Diag_nonzeros] = K_Corr[:, Diag_nonzeros]/D[Diag_nonzeros].T
+    sqrt_inv_nz_Diag = np.zeros(Diag.shape)
+    sqrt_inv_nz_Diag[Diag_nonzeros] = 1 / np.sqrt(Diag[Diag_nonzeros])
+    K_Corr[Diag_nonzeros, :] = K[Diag_nonzeros, :] * sqrt_inv_nz_Diag[Diag_nonzeros][:, np.newaxis]
+    K_Corr[:, Diag_nonzeros] = K_Corr[:, Diag_nonzeros] * sqrt_inv_nz_Diag[Diag_nonzeros][np.newaxis, :]
+    np.fill_diagonal(K_Corr, 1)
     K_Corr = (K_Corr + K_Corr.T)/2
 
     return K_Corr
